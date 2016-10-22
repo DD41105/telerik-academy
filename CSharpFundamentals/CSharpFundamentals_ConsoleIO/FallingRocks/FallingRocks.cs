3157b3e74c0ce4eb9d77bdbfc22aa8a857bf82d3
dimov165 <dimov165@gmail.com>
2016-10-22 20:22:13 +0300

    Implemented rocks; TODO: movement of rocks.

--- CSharpFundamentals/CSharpFundamentals_ConsoleIO/FallingRocks/FallingRocks.cs
+++ CSharpFundamentals/CSharpFundamentals_ConsoleIO/FallingRocks/FallingRocks.cs
@@ -9,9 +9,16 @@ namespace FallingRocks
     {
         static void DrawPlayer(int locationX, int locationY, char player)
         {
-            Console.Clear();
             Console.SetCursorPosition(locationX, locationY);
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.Write(player);
+        }
+
+        static void DrawRock(Rock Rock)
+        {
+            Console.SetCursorPosition(Rock._rockX, Rock._rockY);
+            Console.ForegroundColor = Rock._color;
+            Console.Write(Rock._rock);
         }
 
         static void Main()
@@ -24,17 +31,32 @@ namespace FallingRocks
             int locationY = 24;
             char player = (char)'ʘ';
             Stopwatch stopWatch = new Stopwatch();
+            Random random = new Random();
 
             // console settings
             Console.Title = "Falling Rocks by D Dimov";
             Console.BackgroundColor = ConsoleColor.DarkGray;
             Console.OutputEncoding = Encoding.Unicode;
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.CursorVisible = false;
             Console.BufferHeight = Console.WindowHeight = windowHeight;
             Console.BufferWidth = Console.WindowWidth = windowWidth;
-            
+
+            // create the rocks
+            Rock rock1 = new Rock(random.Next(55), 0, (char)'@', ConsoleColor.Red);
+            Rock rock2 = new Rock(random.Next(55), 0, (char)'#', ConsoleColor.Blue);
+            Rock rock3 = new Rock(random.Next(55), 0, (char)'$', ConsoleColor.Green);
+            Rock rock4 = new Rock(random.Next(55), 0, (char)'%', ConsoleColor.Yellow);
+            Rock rock5 = new Rock(random.Next(55), 0, (char)'&', ConsoleColor.Magenta);
+            Rock rock6 = new Rock(random.Next(55), 0, (char)'*', ConsoleColor.Cyan);
+
+            // initialization of the player and the rocks
             DrawPlayer(27, 24, (char)'ʘ');
+            DrawRock(rock1);
+            DrawRock(rock2);
+            DrawRock(rock3);
+            DrawRock(rock4);
+            DrawRock(rock5);
+            DrawRock(rock6);
 
             ConsoleKeyInfo keyInfo;
             while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape && gameRunning == true)
@@ -46,32 +68,63 @@ namespace FallingRocks
                     case ConsoleKey.RightArrow:
                         if (locationX < 54)
                         {
+                            Console.Clear();
                             locationX++;
+                            DrawPlayer(locationX, locationY, player);
+                            DrawRock(rock1);
+                            DrawRock(rock2);
+                            DrawRock(rock3);
+                            DrawRock(rock4);
+                            DrawRock(rock5);
+                            DrawRock(rock6);
                         }
                         break;
 
                     case ConsoleKey.LeftArrow:
                         if (locationX > 0)
                         {
+                            Console.Clear();
                             locationX--;
+                            DrawPlayer(locationX, locationY, player);
+                            DrawRock(rock1);
+                            DrawRock(rock2);
+                            DrawRock(rock3);
+                            DrawRock(rock4);
+                            DrawRock(rock5);
+                            DrawRock(rock6);
                         }
                         break;
 
                     case ConsoleKey.UpArrow:
                         if (locationY > 0)
                         {
+                            Console.Clear();
                             locationY--;
+                            DrawPlayer(locationX, locationY, player);
+                            DrawRock(rock1);
+                            DrawRock(rock2);
+                            DrawRock(rock3);
+                            DrawRock(rock4);
+                            DrawRock(rock5);
+                            DrawRock(rock6);
                         }
                         break;
 
                     case ConsoleKey.DownArrow:
                         if (locationY < 24)
                         {
+                            Console.Clear();
                             locationY++;
+                            DrawPlayer(locationX, locationY, player);
+                            DrawRock(rock1);
+                            DrawRock(rock2);
+                            DrawRock(rock3);
+                            DrawRock(rock4);
+                            DrawRock(rock5);
+                            DrawRock(rock6);
                         }
                         break;
                 }
-                DrawPlayer(locationX, locationY, player);
             }
 
             // final screen after game ends
---
+++ CSharpFundamentals/CSharpFundamentals_ConsoleIO/FallingRocks/Rock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FallingRocks
+{
+    class Rock
+    {
+        public int _rockX;
+        public int _rockY;
+        public char _rock;
+        public ConsoleColor _color;
+
+        public Rock(int rockX, int rockY, char rock, ConsoleColor color)
+        {
+            this._rockX = rockX;
+            this._rockY = rockY;
+            this._rock = rock;
+            this._color = color;
+        }
+    }
+}
