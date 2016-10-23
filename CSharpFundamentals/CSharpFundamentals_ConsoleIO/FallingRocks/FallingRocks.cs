ee30100b87f4bd8b90fc877cfae01e549f6f5fde
dimov165 <dimov165@gmail.com>
2016-10-23 22:53:27 +0300

    Made a separate Player class; changed way of initialization of the player.

--- CSharpFundamentals/CSharpFundamentals_ConsoleIO/FallingRocks/FallingRocks.cs
+++ CSharpFundamentals/CSharpFundamentals_ConsoleIO/FallingRocks/FallingRocks.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Text;
-using System.Threading;
 using System.Diagnostics;
 
 namespace FallingRocks
 {
     class FallingRocks
     {
-        static void DrawPlayer(int locationX, int locationY, char player)
+        static void DrawPlayer(Player Player)
         {
-            Console.SetCursorPosition(locationX, locationY);
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.Write(player);
+                Console.SetCursorPosition(Player._locationX, Player._locationY);
+                Console.ForegroundColor = Player._playerColor;
+                Console.Write(Player._player);
         }
 
         static void DrawRock(Rock Rock)
@@ -21,15 +20,33 @@ namespace FallingRocks
             Console.Write(Rock._rock);
         }
 
+        static void MoveRock(Rock Rock)
+        {
+            if (Rock._rockY < 24)
+            {
+                Console.SetCursorPosition(Rock._rockX, Rock._rockY);
+                Rock._rockY++;
+            }
+            else
+            {
+                Collision(Rock , 24, true);
+            }   
+        }
+
+        static void Collision(Rock Rock, int locationY, bool gameRunning)
+        {
+            if (Rock._rockY == locationY && gameRunning == true)
+            {
+                gameRunning = false;
+            }
+        }
+
         static void Main()
         {
             // define the variables
             bool gameRunning = true;
             int windowHeight = 25;
             int windowWidth = 55;
-            int locationX = 27;
-            int locationY = 24;
-            char player = (char)'ʘ';
             Stopwatch stopWatch = new Stopwatch();
             Random random = new Random();
 
@@ -41,16 +58,18 @@ namespace FallingRocks
             Console.BufferHeight = Console.WindowHeight = windowHeight;
             Console.BufferWidth = Console.WindowWidth = windowWidth;
 
-            // create the rocks
+            // create the rocks 
+            // TODO: change the variables in the Collision method so that it works correctly
             Rock rock1 = new Rock(random.Next(55), 0, (char)'@', ConsoleColor.Red);
             Rock rock2 = new Rock(random.Next(55), 0, (char)'#', ConsoleColor.Blue);
             Rock rock3 = new Rock(random.Next(55), 0, (char)'$', ConsoleColor.Green);
             Rock rock4 = new Rock(random.Next(55), 0, (char)'%', ConsoleColor.Yellow);
             Rock rock5 = new Rock(random.Next(55), 0, (char)'&', ConsoleColor.Magenta);
             Rock rock6 = new Rock(random.Next(55), 0, (char)'*', ConsoleColor.Cyan);
+            Player player1 = new Player(27, 24, (char)'ʘ', ConsoleColor.Blue);
 
             // initialization of the player and the rocks
-            DrawPlayer(27, 24, (char)'ʘ');
+            DrawPlayer(player1);
             DrawRock(rock1);
             DrawRock(rock2);
             DrawRock(rock3);
@@ -58,82 +77,126 @@ namespace FallingRocks
             DrawRock(rock5);
             DrawRock(rock6);
 
+            stopWatch.Start();
+
             ConsoleKeyInfo keyInfo;
             while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape && gameRunning == true)
             {
-                Console.Write(player);
-                stopWatch.Start();
+                Console.Write(player1._player);
                 switch (keyInfo.Key)
                 {
                     case ConsoleKey.RightArrow:
-                        if (locationX < 54)
+                       if (player1._locationX < 54)
                         {
                             Console.Clear();
-                            locationX++;
-                            DrawPlayer(locationX, locationY, player);
+                            player1._locationX++;
+                            DrawPlayer(player1);
                             DrawRock(rock1);
                             DrawRock(rock2);
                             DrawRock(rock3);
                             DrawRock(rock4);
                             DrawRock(rock5);
                             DrawRock(rock6);
+                            MoveRock(rock1);
+                            MoveRock(rock2);
+                            MoveRock(rock3);
+                            MoveRock(rock4);
+                            MoveRock(rock5);
+                            MoveRock(rock6);
+                        }
+                        else
+                        {
+                            player1._locationX--;
                         }
                         break;
 
                     case ConsoleKey.LeftArrow:
-                        if (locationX > 0)
+                        if (player1._locationX > 0)
                         {
                             Console.Clear();
-                            locationX--;
-                            DrawPlayer(locationX, locationY, player);
+                            player1._locationX--;
+                            DrawPlayer(player1);
                             DrawRock(rock1);
                             DrawRock(rock2);
                             DrawRock(rock3);
                             DrawRock(rock4);
                             DrawRock(rock5);
                             DrawRock(rock6);
+                            MoveRock(rock1);
+                            MoveRock(rock2);
+                            MoveRock(rock3);
+                            MoveRock(rock4);
+                            MoveRock(rock5);
+                            MoveRock(rock6);
+                        }
+                        else
+                        {
+                            player1._locationX++;
                         }
                         break;
 
                     case ConsoleKey.UpArrow:
-                        if (locationY > 0)
+                        if (player1._locationY > 0)
                         {
                             Console.Clear();
-                            locationY--;
-                            DrawPlayer(locationX, locationY, player);
+                            player1._locationY--;
+                            DrawPlayer(player1);
                             DrawRock(rock1);
                             DrawRock(rock2);
                             DrawRock(rock3);
                             DrawRock(rock4);
                             DrawRock(rock5);
                             DrawRock(rock6);
+                            MoveRock(rock1);
+                            MoveRock(rock2);
+                            MoveRock(rock3);
+                            MoveRock(rock4);
+                            MoveRock(rock5);
+                            MoveRock(rock6);
+                        }
+                        else
+                        {
+                            gameRunning = false;
                         }
                         break;
 
                     case ConsoleKey.DownArrow:
-                        if (locationY < 24)
+                        if (player1._locationY < 24)
                         {
                             Console.Clear();
-                            locationY++;
-                            DrawPlayer(locationX, locationY, player);
+                            player1._locationY++;
+                            DrawPlayer(player1);
                             DrawRock(rock1);
                             DrawRock(rock2);
                             DrawRock(rock3);
                             DrawRock(rock4);
                             DrawRock(rock5);
                             DrawRock(rock6);
+                            MoveRock(rock1);
+                            MoveRock(rock2);
+                            MoveRock(rock3);
+                            MoveRock(rock4);
+                            MoveRock(rock5);
+                            MoveRock(rock6);
+                        }
+                        else
+                        {
+                            gameRunning = false;
                         }
                         break;
                 }
             }
 
-            // final screen after game ends
-            stopWatch.Stop();
-            TimeSpan ts = stopWatch.Elapsed;
-            Console.Clear();
-            Console.WriteLine("");
-            Console.WriteLine("Time survived: " + ts);
-            Console.WriteLine("");
+            if (gameRunning == false)
+            {
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;
+                Console.Clear();
+                Console.WriteLine("");
+                Console.WriteLine("Time survived: {0} minutes {1} seconds", ts.Minutes, ts.Seconds);
+                Console.WriteLine("");
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ CSharpFundamentals/CSharpFundamentals_ConsoleIO/FallingRocks/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FallingRocks
+{
+    class Player
+    {
+        public int _locationX;
+        public int _locationY;
+        public char _player;
+        public ConsoleColor _playerColor;
+
+        public Player(int locationX, int locationY, char player, ConsoleColor playerColor)
+        {
+            this._locationX = locationX;
+            this._locationY = locationY;
+            this._player = player;
+            this._playerColor = playerColor;
+        }
+    }
+}
