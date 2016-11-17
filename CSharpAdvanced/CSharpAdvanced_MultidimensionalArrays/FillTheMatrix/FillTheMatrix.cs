4a37a56db9783c2af68dfbb8efc7fe6a28f5e9a8
dimov165 <dimov165@gmail.com>
2016-11-17 12:50:58 +0200

    Added homeworks to C# Advanced lecture 1.

---
+++ CSharpAdvanced/CSharpAdvanced_MultidimensionalArrays/FillTheMatrix/FillTheMatrix.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace FillTheMatrix
+{
+    class FillTheMatrix
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            char type = char.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+            int counter = 1;
+
+            if (type == 'a')
+            {
+                for (int row = 0; row < size; row++)
+                {
+                    for (int col = 0; col <= size; col++)
+                    {
+                        if (col == size)
+                        {
+                            counter = row + 2;
+                            break;
+                        }
+                        matrix[row, col] = counter;
+                        counter += size;
+                    }
+                }
+            }
+            else if (type == 'b')
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    for (int row = 0; row < size; row++, counter++)
+                    {
+                        if ((col % 2 == 0))
+                        {
+                            matrix[row, col] = counter;
+                        }
+                        else
+                        {
+                            matrix[row, col] = counter + (size - (row * 2) - 1);
+                        }
+                    }
+                }
+            }
+            else if (type == 'c')
+            {
+                // first half of the matrix
+                for (int reversedRow = size - 1; reversedRow >= 0; reversedRow--)
+                {
+                    for (int col = 0; col < (size - reversedRow); col++, counter++)
+                    {
+                        if (col == 0)
+                        {
+                            matrix[reversedRow, col] = counter;
+                        }
+                        else
+                        {
+                            matrix[(reversedRow + col), col] = counter;
+                        }
+                    }
+                }
+                // second half of the matrix
+                for (int col = 1; col < size; col++)
+                {
+                    for (int row = 0; row < (size - col); row++, counter++)
+                    {
+                        if (row == 0)
+                        {
+                            matrix[row, col] = counter;
+                        }
+                        else
+                        {
+                            matrix[row, (col + row)] = counter;
+                        }
+                    }
+                }
+
+            }
+            else if (type == 'd')
+            {
+                for (int depth = 0; depth <= (size / 2); depth++)
+                {
+                    for (int down = depth; down <= size - depth - 1; down++, counter++)
+                    {
+                        matrix[down, depth] = counter;
+                    }
+                    for (int right = depth + 1; right <= size - depth - 1; right++, counter++)
+                    {
+                        matrix[(size - depth - 1), right] = counter;
+                    }
+                    for (int up = size - depth - 2; up > depth; up--, counter++)
+                    {
+                        matrix[up, (size - depth - 1)] = counter;
+                    }
+                    for (int left = size - depth - 1; left > depth; left--, counter++)
+                    {
+                        if (matrix[depth, left] == 0)
+                        {
+                            matrix[depth, left] = counter;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // print the matrix
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    // if it's the last element write it without a space at the end
+                    if (col == size - 1)
+                    {
+                        Console.Write(matrix[row, col]);
+                    }
+                    else // other elements go here
+                    {
+                        Console.Write("{0} ", matrix[row, col]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced/CSharpAdvanced_MultidimensionalArrays/MaximalSum/MaximalSum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximalSum
+{
+    class MaximalSum
+    {
+        static void Main()
+        {
+            string[] size = Console.ReadLine().Split(' ');
+            int N = int.Parse(size[0]);
+            int M = int.Parse(size[1]);
+            int[,] matrix = new int[N, M];
+            int[,] subMatrix = new int[3, 3];
+            int maxSum = int.MinValue;
+            int tempSum = 0;
+
+            for (int row = 0; row < N; row++)
+            {
+                int[] tempArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < M; col++)
+                {
+                    matrix[row, col] = tempArr[col];
+                }
+                Array.Clear(tempArr, 0, tempArr.Length);
+            }
+
+            for (int row = 0; row < (N-2); row++)
+            {
+                for (int col = 0; col < (M - 2); col++)
+                {
+                    for (int subRow = row; subRow < (row + 3); subRow++)
+                    {
+                        for (int subCol = col; subCol < (col + 3); subCol++)
+                        {
+                            tempSum += matrix[subRow, subCol];
+                        }
+                    }
+                    if (tempSum > maxSum)
+                    {
+                        maxSum = tempSum;
+                    }
+                    tempSum = 0;
+                }
+            }
+
+            Console.WriteLine(maxSum);
+        }
+    }
+}
