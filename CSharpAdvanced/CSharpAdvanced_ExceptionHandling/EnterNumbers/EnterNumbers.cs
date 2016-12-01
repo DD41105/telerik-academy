887d328803eaa96a9aaf5d6f91f5a6004ed1ac9d
dimov165 <dimov165@gmail.com>
2016-12-01 11:18:17 +0200

    Exception handling HW

---
+++ CSharpAdvanced/CSharpAdvanced_ExceptionHandling/EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EnterNumbers
+{
+    class EnterNumbers
+    {
+        private static int ReadNumber(int start, int end)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number <= start || number >= end)
+            {
+                throw new System.ArgumentOutOfRangeException(String.Format("Integer {0} should be between {1} and {2}", number, start, end));
+            }
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+            try
+            {
+                int[] arr = new int[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    arr[i] = ReadNumber(i <= 0 ? start : arr[i - 1], end);
+                }
+                Console.Write("{0} < ", 1);
+                foreach (var item in arr)
+                {
+                    Console.Write("{0} < ", item);
+                }
+                Console.WriteLine(100);
+            }
+            catch
+            {
+                Console.WriteLine("Exception");
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced/CSharpAdvanced_ExceptionHandling/SquareRoot/SquareRoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            try
+            {
+                double input = double.Parse(Console.ReadLine());
+                if (input < 0)
+                {
+                    Console.WriteLine("Invalid number");
+                }
+                else
+                {
+                    double result = Math.Sqrt(input);
+                    Console.WriteLine("{0:F3}", result);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
