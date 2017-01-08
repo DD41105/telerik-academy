391498a2cfd9c95527790bcf06e0155b3c47b45b
dimov165 <dimov165@gmail.com>
2017-01-08 15:03:20 +0200

    JS HW 4 (Arrays).

---
+++ JS Fundamentals/JS_HW_4/02. Lexicographically comparison/script.js
@@ -0,0 +1,14 @@
+function solve(args) {
+    var arr = args[0].split('\n'),
+        firstStr = arr[0],
+        secondStr = arr[1];
+
+    if (firstStr > secondStr) {
+        console.log('>');
+    } else if (firstStr < secondStr) {
+        console.log('<');
+    } else {              // firstStr === secondStr
+        console.log('=');
+    }
+
+}
