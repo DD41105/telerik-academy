2adffd5c4d969110f58f413c12e254405188092a
dimov165 <dimov165@gmail.com>
2017-03-18 10:53:39 +0200

    Added HW 4 of the High Quality Code Part 1 course.

---
+++ High Quality Code - Part 1/04. Variables, Expressions, Constants/Task 1. Class Size/Size.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task1._Class_Size
+{
+    public class Size
+    {
+        public double Width, Height;
+
+        public Size(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public static Size GetRotatedSize(Size s, double angle)
+        {
+            return new Size(Math.Abs(Math.Cos(angle)) * s.Width + Math.Abs(Math.Sin(angle)) * s.Height,
+                            Math.Abs(Math.Sin(angle)) * s.Width + Math.Abs(Math.Cos(angle)) * s.Height);
+        }
+    }
+}
---
+++ High Quality Code - Part 1/04. Variables, Expressions, Constants/Task 2. Method PrintStatistics/PrintStatisticsClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task_2.Method_PrintStatistics
+{
+    class PrintStatisticsClass
+    {
+        public void PrintStatistics(double[] arrayToCheck, int count)
+        {
+            // initializa the max as being equal to the first element of the array, then check if any of the others is bigger
+            double max = arrayToCheck[0];
+            for (int i = 0; i < count; i++)
+            {
+                if (arrayToCheck[i] > max)
+                {
+                    max = arrayToCheck[i];
+                }
+            }
+            Console.WriteLine("Max: {0}", max);
+
+            // a way to get the minimum - initialize the minimum as being equal to the first element of the array; then check if any of the others is smaller
+            double min = arrayToCheck[0];
+            for (int i = 0; i < count; i++)
+            {
+                if (arrayToCheck[i] < min)
+                {
+                    min = arrayToCheck[i];
+                }
+            }
+            Console.WriteLine("Min: {0}", min);
+
+            // a way to get the sum
+            double sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                sum += arrayToCheck[i];
+            }
+            Console.WriteLine("Sum: {0}", sum);
+
+            // a way to get the average (tmp / count)
+            double average = sum / count;
+            Console.WriteLine("Average: {0}", average);
+        }
+    }
+}
