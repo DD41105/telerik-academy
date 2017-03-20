60fe3517c9a74b601b917f126c6f733f3c575d8f
dimov165 <dimov165@gmail.com>
2017-03-20 11:54:05 +0200

    Added HW 6 and 7.

---
+++ High Quality Code - Part 1/06. High Quality Methods/Task 1. Quality Methods/Methods/Methods.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Methods
+{
+    class Methods
+    {
+        static double CalcTriangleArea(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                Console.Error.WriteLine("Sides should be positive.");
+                return -1;
+            }
+            double s = (a + b + c) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return area;
+        }
+
+        // The way that it was written was wrong; it asks for a number and it should return a digit; rewritten
+        static int NumberToDigit(string number)
+        {
+            switch (number)
+            {
+                case "zero": return 0;
+                case "one": return 1;
+                case "two": return 2;
+                case "three": return 3;
+                case "four": return 4;
+                case "five": return 5;
+                case "six": return 6;
+                case "seven": return 7;
+                case "eight": return 8;
+                case "nine": return 9;
+                default: throw new ArgumentException("Not a valid number");
+            }
+        }
+
+        // added a max integer, looks better and is easier to understand this way
+        static int FindMax(params int[] elements)
+        {
+            int max = 0;
+            if (elements == null || elements.Length == 0)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (elements[i] > max)
+                {
+                    max = elements[i];
+                }
+            }
+            return max;
+        }
+
+        // Added the exception for a wrong format
+        static void PrintAsNumber(object number, string format)
+        {
+            if (format == "f")
+            {
+                Console.WriteLine("{0:f2}", number);
+            }
+            if (format == "%")
+            {
+                Console.WriteLine("{0:p0}", number);
+            }
+            if (format == "r")
+            {
+                Console.WriteLine("{0,8}", number);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid format");
+            }
+        }
+
+        static double CalcDistance(double x1, double y1, double x2, double y2, 
+            out bool isHorizontal, out bool isVertical)
+        {
+            isHorizontal = (y1 == y2);
+            isVertical = (x1 == x2);
+
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            return distance;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(CalcTriangleArea(3, 4, 5));
+            
+            Console.WriteLine(NumberToDigit("five"));
+            
+            Console.WriteLine(FindMax(5, -1, 3, 2, 14, 2, 3));
+            
+            PrintAsNumber(1.3, "f");
+            PrintAsNumber(0.75, "%");
+            PrintAsNumber(2.30, "r");
+
+            bool horizontal, vertical;
+            Console.WriteLine(CalcDistance(3, -1, 3, 2.5, out horizontal, out vertical));
+            Console.WriteLine("Horizontal? " + horizontal);
+            Console.WriteLine("Vertical? " + vertical);
+
+            Student peter = new Student() { FirstName = "Peter", LastName = "Ivanov" };
+            peter.OtherInfo = "From Sofia, born on 17.03.1992";
+
+            Student stella = new Student() { FirstName = "Stella", LastName = "Markova" };
+            stella.OtherInfo = "From Vidin, gamer, high results, born on 03.11.1993";
+
+            Console.WriteLine("{0} older than {1} -> {2}",
+                peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
+        }
+    }
+}
